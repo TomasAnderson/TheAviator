79a186617bcb08c90142c39a3ea836869e588e50
Yuri Zhou <t_zhouy@ads.autodesk.com>
2016-06-13 17:37:06 +0800

    create the scene

---
+++ js/main.js
@@ -0,0 +1,81 @@
+'use strict';
+
+//color palette
+var Colors = {
+  red:0xf25346,
+  white:0xd8d0d1,
+  brown:0x59332e,
+  pink:0xF5986E,
+  brownDark:0x23190f,
+  blue:0x68c3c0,
+};
+
+// Three.js variables
+var scene,
+    camera, fieldOfView, aspectRatio, nearPlane, farPlane,
+    renderer, container;
+
+// Screen variables
+var HEIGHT, WIDTH;
+
+function createScene () {
+   HEIGHT = window.innerHeight;
+   WIDTH = window.innerWidth;
+
+   //1. create the scene
+   scene = new THREE.Scene();
+   scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+
+   //2. create a camera
+   fieldOfView = 60;
+   aspectRatio = WIDTH / HEIGHT;
+   nearPlane = 1;
+   farPlane = 1000;
+   camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
+
+   camera.position.x = 0;
+   camera.position.y = 100;
+   camera.position.z = 200;
+
+   //3. create the renderer
+   renderer = new THREE.WebGLRenderer({
+    alpha:true,
+    antialias: true
+   });
+   renderer.setSize(WIDTH, HEIGHT);
+   renderer.shadowMap.enabled = true;
+
+   //4. add into HTML
+   container = document.getElementById('world');
+   container.appendChild(renderer.domElement);
+
+   //5. make the scene resizale
+   window.addEventListener('resize', function () {
+      HEIGHT = window.innerHeight;
+      WIDTH = window.innerWidth;
+      //update renderer
+      renderer.setSize(WIDTH, HEIGHT);
+      //update camera
+      camera.aspect = WIDTH / HEIGHT;
+      camera.updateProjectionMatrix(); 
+   }, false);
+}
+
+
+
+
+function init () {
+  createScene();
+  // createLights();
+  // createSea();
+  // createSky();
+  // createPlane();
+
+  //render
+  renderer.render(scene, camera);
+}
+
+
+window.addEventListener('load', init, false);
+
+
